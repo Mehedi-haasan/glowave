757df7dc066d7d254e58523cc6430f3e59e1bd96
Mehedi Hasan <130404728+Mehedi-haasan@users.noreply.github.com>
2025-01-15 21:57:48 +0600

    change

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/glowave',
-  assetPrefix: '/glowave',
-
+module.exports = {
   images: {
-    domains: ['d3aj5vjnhssdu4.cloudfront.net', 's3-alpha-sig.figma.com'],
+      domains: ['d3aj5vjnhssdu4.cloudfront.net', 's3-alpha-sig.figma.com'],
   },
+  output: "export",
 };
-
-module.exports = nextConfig;
