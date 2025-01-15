496e8f9d5a0647aa83bf9dc5eb900fb011355089
Mehedi Hasan <130404728+Mehedi-haasan@users.noreply.github.com>
2025-01-15 21:52:28 +0600

    add libra

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-    images: {
-      domains: ['d3aj5vjnhssdu4.cloudfront.net', 's3-alpha-sig.figma.com'],
-    },
-  };
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/glowave',
+  assetPrefix: '/glowave',
+
+  images: {
+    domains: ['d3aj5vjnhssdu4.cloudfront.net', 's3-alpha-sig.figma.com'],
+  },
+};
+
+module.exports = nextConfig;
